391462769416b6a96946d1d0e9b6d219023500ad
miraweingarten <91880104+miraweingarten@users.noreply.github.com>
2023-10-18 20:22:35 +0300

    stage1 page 16

---
+++ DalFacade/DO/Task.cs
@@ -0,0 +1,19 @@
+﻿namespace DO;
+
+public record Task
+(
+    int Id,
+    string Description,
+    string Alias,
+    bool Milestone,
+    DateTime CreatedAt,
+    DateTime? Start,
+    DateTime? ScheduledDate,
+    DateTime? DeadLine,
+    DateTime? Complete,
+    string? ProductDescription,
+    string? Remarks,
+    int? EngineerId,
+    EngineerExperience? CopmlexityLevel
+);
+
