7bfae7afb13d41c780983c813d7777414b2b8555
TziporaShenker <tzp81134@gmail.com>
2024-01-08 21:31:16 +0200

    Milestone V

--- BL/BlApi/IMilestone.cs
+++ BL/BlApi/IMilestone.cs
@@ -5,7 +5,7 @@ namespace BlApi;
 
 public interface IMilestone
 {
-    List<TaskInList>? Create(); //Creates new entity object in DAL
+   void Create(); //Creates new entity object in DAL
     BO.Milestone? Read(int id); //Reads entity object by its ID 
     void Update(BO.Milestone item); //Updates entity object
 }
--- BL/BlImplementation/MilestoneImplementation.cs
+++ BL/BlImplementation/MilestoneImplementation.cs
@@ -3,17 +3,14 @@ using BlApi;
 using BO;
 using DalApi;
 using DO;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace BlImplementation;
 
 internal class MilestoneImplementation : IMilestone
 {
     private DalApi.IDal _dal = DalApi.Factory.Get;
-    public List<BO.TaskInList>? Create()
+    public void Create()
     {
         var groupedDependencies = _dal.Dependency.ReadAll()
         .OrderBy(dep => dep?.DependsOnTask)
@@ -26,40 +23,10 @@ internal class MilestoneImplementation : IMilestone
        .GroupBy(dep => dep.Dependencies,
         (key, group) => group.First())
        .ToList();
-        //var distinctDependencies2 = new List<dynamic>();
 
-        //foreach (var depGroup in groupedDependencies)
-        //{
-        //    var existingDep = distinctDependencies.FirstOrDefault(d => Enumerable.SequenceEqual(d.Dependencies, depGroup.Dependencies));
-
-        //    if (existingDep == null)
-        //    {
-        //        distinctDependencies.Add(depGroup);
-        //    }
-        //}
-
-        //var groupedDependencies =
-        //from dependency in _dal.Dependency.ReadAll()
-        //group dependency by (dependency.DependentTask,dependency.DependsOnTask) into (DependentTask, DependsOnTask)=> newGroup
-        //orderby newGroup.DependentTask
-        //select new { Key = newGroup.Key, List = new List<TaskInList> };
-
-        //var distinctDependencies = groupedDependencies
-        //   .SelectMany(depGroup => depGroup.Dependencies)
-        //   .Where(dep => dep != null)
-        //   .Distinct()
-        //   .ToList();
-        //
-
-        //    var distinctDependencies = groupedDependencies
-        //.SelectMany(depGroup => depGroup.Dependencies.
-        //Select(dep => new { TaskId = depGroup.TaskId, DependencyId = dep }))
-        //.Where(dep => dep.DependencyId != null)
-        //.Distinct()
-        //.ToList();
-        int id = 0;
-        var mileStone =
-        from dependencies in distinctDependencies
+        int id = 1;
+        var mileStones =
+        from distinctDependency in distinctDependencies
         select new BO.Task()
         {
             Id = id++,
@@ -67,50 +34,166 @@ internal class MilestoneImplementation : IMilestone
             Description = null,
             CreatedAtDate = DateTime.Today,
             Status = Status.Scheduled,//נדרש חישוב
-            Dependencies = (from dep in dependencies.Dependencies
-                           let task = _dal.Task.Read(dep)
-                           select new BO.TaskInList()
-                           {
-                               Id = task.Id,
-                               Description= task.Description,
-                               Alias = task.Alias,
-                               Status = null,
-                           }).ToList(),
+            Dependencies = (from dep in distinctDependency.Dependencies
+                            let task = _dal.Task.Read(dep)
+                            select new BO.TaskInList()
+                            {
+                                Id = task.Id,
+                                Description = task.Description,
+                                Alias = task.Alias,
+                                Status = null,
+                            }).ToList(),
 
-        StartDate = null,//נדרש חישוב
+            StartDate = null,//נדרש חישוב
             ForecastDate = null,//נדרש חישוב
             CompleteDate = null,//נדרש חישוב,
             DeadlineDate = null,//נדרש חישוב
             Remarks = null,//נדרש חישוב
             Milestone = null,//נדרש חישוב
-            RequiredEffortTime= null,//נדרש חישוב
-            Deliverables= null,//נדרש חישוב
-            Engineer= null, 
-            Copmlexity= null,
+            RequiredEffortTime = null,//נדרש חישוב
+            Deliverables = null,//נדרש חישוב
+            Engineer = null,
+            Copmlexity = null,
         };
 
+        var dalMileStones =
+       from mileStone in mileStones
+       select new DO.Task()
+       {
+           Id = id++,
+           Alias = mileStone.Alias,
+           Description = mileStone.Description,
+           CreatedAtDate = mileStone.CreatedAtDate,
+           RequiredEffortTime = mileStone.RequiredEffortTime,
+           IsMilestone = true,
+           StartDate = mileStone.StartDate,//נדרש חישוב
+           ScheduledDate = mileStone.ScheduledDate,//נדרש חישוב
+           DeadlineDate = mileStone.DeadlineDate,//נדרש חישוב
+           CompleteDate = mileStone.CompleteDate,//נדרש חישוב,
+           Deliverables = mileStone.Deliverables,//נדרש חישוב
+           Remarks = mileStone.Remarks,//נדרש חישוב
+           EngineerId = null,
+           Copmlexity = null,
+       };
 
-        //return (
-        //        from DO.Dependency doDependency in _dal.Dependency.ReadAll()
-        //        where (doDependency.DependentTask == taskId)
-        //        let dependentTask = Read(doDependency.DependsOnTask)
-        //        select new BO.TaskInList()
-        //        {
-        //            Id = doDependency.DependsOnTask,
-        //            Description = dependentTask.Description,
-        //            Alias = dependentTask.Alias,
-        //            Status = null,
-        //        }).ToList();
+        dalMileStones.ToList().ForEach(dalMileStone => _dal.Task.Create(dalMileStone));
+        var updatedDependencies = from mileStone in mileStones
+                                  from dependency in mileStone.Dependencies
+                                  from dalDependency in _dal.Dependency.ReadAll(dep => dep.DependsOnTask == dependency.Id)
+                                  let dalMilestone = _dal.Task.Read(t => t.Alias == mileStone.Alias)
+                                  select new Dependency(dalDependency.Id, dalDependency.DependentTask, dalMilestone.Id);
+
+        updatedDependencies.ToList().ForEach(updatedDependency => _dal.Dependency.Update(updatedDependency));
+  
+        var dalStartMilestone = new DO.Task()
+        {
+            Id = id,
+            Alias = "START",
+            Description = null,
+            CreatedAtDate = DateTime.Today,
+            StartDate = null,//נדרש חישוב
+            ScheduledDate = null,//נדרש חישוב
+            CompleteDate = null,//נדרש חישוב,
+            DeadlineDate = null,//נדרש חישוב
+            Remarks = null,//נדרש חישוב
+            IsMilestone = true,//נדרש חישוב
+            RequiredEffortTime = null,//נדרש חישוב
+            Deliverables = null,//נדרש חישוב
+            EngineerId = null,
+            Copmlexity = null,
+        };
+        _dal.Task.Create(dalStartMilestone);
+
+        var dalBeginMilestone = _dal.Task.Read(t => t.Alias == dalStartMilestone.Alias);
+
+        var startDependencies = from task in _dal.Task.ReadAll()
+                              let dependency = _dal.Dependency.ReadAll(dep => dep.DependentTask == task.Id)
+                              where dependency == null
+                              select _dal.Dependency.Create(new Dependency(0, task.Id, dalBeginMilestone.Id));
+     
+        var dalEndMilestone = new DO.Task()
+        {
+            Id = id,
+            Alias = "END",
+            Description = null,
+            CreatedAtDate = DateTime.Today,
+            StartDate = null,//נדרש חישוב
+            ScheduledDate = null,//נדרש חישוב
+            CompleteDate = null,//נדרש חישוב,
+            DeadlineDate = null,//נדרש חישוב
+            Remarks = null,//נדרש חישוב
+            IsMilestone = true,//נדרש חישוב
+            RequiredEffortTime = null,//נדרש חישוב
+            Deliverables = null,//נדרש חישוב
+            EngineerId = null,
+            Copmlexity = null,
+        };
+
+        _dal.Task.Create(dalEndMilestone);
+        var endDependencies = (from task in _dal.Task.ReadAll()
+                            let dependency = _dal.Dependency.ReadAll((dep => dep.DependsOnTask == task.Id))
+                            where dependency == null
+                            select new BO.TaskInList()
+                            {
+                                Id = task.Id,
+                                Description = task.Description,
+                                Alias = task.Alias,
+                                Status = null,
+                            }).ToList();
+
+        var dalFinishMilestone = _dal.Task.Read(t => t.Alias == dalEndMilestone.Alias);
+        foreach (var dependency in endDependencies)
+        {
+            _dal.Dependency.Create(new Dependency(0, dependency.Id, dalFinishMilestone.Id));
+        }
     }
 
     public Milestone? Read(int id)
     {
-        throw new NotImplementedException();
-    }
+        var milestone = _dal.Task.Read(id);
+        var dependencies = _dal.Dependency.ReadAll(dep => dep.DependsOnTask == id);
+        var dependenciesList = (from dependency in dependencies
+                                let task = _dal.Task.Read(dependency.DependentTask)
+                                select new BO.TaskInList()
+                                {
+                                    Id = task.Id,
+                                    Description = task.Description,
+                                    Alias = task.Alias,
+                                    Status = null,
+                                }).ToList();
 
+        return new BO.Milestone()
+        {
+            Id = id,
+            Description = milestone.Description,
+            Alias = milestone.Alias,
+            CreatedAtDate = milestone.CreatedAtDate,
+            Status = Status.Scheduled,//נדרש חישוב
+            StartDate = null,//נדרש חישוב
+            ForecastDate = null,//נדרש חישוב          
+            DeadlineDate = null,//נדרש חישוב
+            CompleteDate = null,//נדרש חישוב,
+            CompletionPercentage = dependenciesList.Count(dep => dep.Status == Status.Done) / dependenciesList.Count(),
+            Remarks = null,//נדרש חישוב
+            Dependencies = dependenciesList,
+        };
+    }
     public void Update(Milestone item)
     {
-        throw new NotImplementedException();
+        if (Read(item.Id) is null)
+            throw new BO.BlDoesNotExistException($"Milestone with ID={item.Id} does Not exist");
+
+        DO.Task doMilestone = new DO.Task
+                (item.Id, item.Alias, item.Description, item.CreatedAtDate,null, true, item.StartDate, null, item.DeadlineDate, item.CompleteDate, null, item.Remarks, null, null);
+        try
+        {
+            _dal.Task.Update(doMilestone);
+        }
+        catch (Exception ex)
+        {
+
+            throw new BO.BlDoesNotExistException(ex.Message, ex);
+        }
     }
 
 }
