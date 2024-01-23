b850c4cb1e59f474d65abdd5a91e3438ca8b1450
estiww <esti69771@gmail.com>
2024-01-23 14:34:19 +0200

    Step 5 not perfect

--- PL/Engineer/EngineerListWindow.xaml.cs
+++ PL/Engineer/EngineerListWindow.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace PL.Engineer
 {
@@ -38,7 +28,7 @@ namespace PL.Engineer
 
         private void CBEngineerExperience_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            EngineerList = (EngineerExperience == BO.EngineerExperience.None) ?
+            EngineerList = (EngineerExperience == BO.EngineerExperience.None)?
                 s_bl?.Engineer.ReadAll()! : s_bl?.Engineer.ReadAll(item => item.Level == EngineerExperience)!;
         }
 
